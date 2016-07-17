863b65d7943977215eadb273df6dcdddb9470247
Nilpo <devnilpo@gmail.com>
2016-07-17 03:44:31 -0400

    adding HIBP class

---
+++ settings.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace HIBP;
+
+/**
+ * Defines a constant for the base URL of the HaveIBeenPwned API.
+ *     GET https://haveibeenpwned.com/api/{service}/{parameter}
+ *     api-version: 2
+ */
+const API_URL = 'https://haveibeenpwned.com/api/';
+
+/**
+ * Defines the current API version.
+ */
+const API_VERSION = '2';
+
+/**
+ * Determines whether the API version should be specified by supplying a custom
+ * 'api-version' header or by using content negotiation. The default value of
+ * false indicates that requests should use the custom header.
+ */
+const USE_CONTENT_NEGOTIATION = false;
+
+/**
+ * If content negotiation is implemented, specifies the value for the accept header.
+ */
+const ACCEPT_HEADER = 'application/vnd.haveibeenpwned.v2+json';
+
+/**
+ * Each request to the API must be accompanied by a user agent request header.
+ * Typically, this should be the name of the app consuming the service. For
+ * example, "Pwnage-Checker-For-PHP". A missing user agent header will result
+ * in an HTTP 403 response.
+ */
+const USER_AGENT = '';
+
+/**
+ * Boolean value indicating whether responses should be returned as arrays. The default
+ * is to parse them into PHP objects.
+ */
+const RESULT_AS_ARRAY = false;
+
+// END
---
+++ src/HIBP.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace HIBP;
+
+use \Httpful\Request;
+
+require_once(__DIR__ . '/../settings.php');
+
+class HIBP {
+
+	/**
+	 * Returns a list of all breaches that a particular account has been involved in.
+	 * 
+	 * @param string An email or user name to check.
+	 * @param boolean Indicates whether the response should be truncated, optional.
+	 * @param string Filters results to the specified domain, optional.
+	 * @return mixed Returns an empty string or an array of objects.
+	 */
+	public static function getAllBreaches($account, $truncate = false, $domain = null) {
+		$uri = API_URL . "breachedaccount/{$account}?";
+		$params = array();
+
+		if ($truncate === true) {
+			$params['truncateResponse'] = true;
+		}
+
+		if ($domain !== null) {
+			$params['domain'] = $domain;
+		}
+
+		if (count($params)) {
+			$uri .= http_build_query($params);
+		}
+
+		return self::sendRequest($uri);
+	}
+
+	/**
+	 * Returns the details of each breach in the system.
+	 * 
+	 * @param string Filters results to the specified domain, optional.
+	 * @return mixed Returns an empty string or an array of objects.
+	 */
+	public static function getAllBreachedSites($domain = null) {
+		$uri = API_URL . "breaches?";
+		$params = array();
+
+		if ($domain !== null) {
+			$params['domain'] = $domain;
+		}
+
+		if (count($params)) {
+			$uri .= http_build_query($params);
+		}
+
+		return self::sendRequest($uri);
+	}
+
+	/**
+	 * Returns the details of a single breach.
+	 * 
+	 * @param string The "name" of the breach to return.
+	 * @return mixed Returns an empty string or an array of objects.
+	 */
+	public static function getBreachedSite($name) {
+		$uri = API_URL . "breach/{$name}";
+
+		return self::sendRequest($uri);
+	}
+
+	/**
+	 * Returns an alphabetical list of data classes that represent the data that was
+	 * compromised during a breach.
+	 * 
+	 * @return array Returns a string array of data classes.
+	 */
+	public static function getDataClasses() {
+		$uri = API_URL . "dataclasses";
+
+		return self::sendRequest($uri);
+	}
+
+	/**
+	 * Returns all pastes associated with a specified email address.
+	 * 
+	 * @param string An email address to check.
+	 * @return mixed Returns an empty string or an array of objects.
+	 */
+	public static function getAllPastes($account) {
+		$uri = API_URL . "pasteaccount/{$account}?";
+
+		return self::sendRequest($uri);
+	}
+
+	/**
+	 * Sends an HTTP request and returns a response.
+	 * 
+	 * @param string A valid API URI.
+	 * @return mixed Returns an empty string or an array of objects.
+	 */
+	private static function sendRequest($uri) {
+		if (RESULT_AS_ARRAY === true) {
+			\Httpful\Httpful::register(\Httpful\Mime::JSON, new \Httpful\Handlers\JsonHandler(array('decode_as_array' => true)));
+		}
+
+		$request = Request::get($uri);
+
+		if (USE_CONTENT_NEGOTIATION === false) {
+			$request->addHeaders(array(
+				'api-version' => API_VERSION,
+				'User-Agent' => USER_AGENT
+			));
+		} else {
+			$request->addHeaders(array(
+				'Accept' => ACCEPT_HEADER,
+				'User-Agent' => USER_AGENT
+			));
+		}
+
+		$response = $request->send();
+
+		self::checkResponseCode($response);
+
+		return $response->body;
+	}
+
+	/**
+	 * Checks the HTTP response code for the response and throws an exception in the
+	 * event of an error.
+	 * 
+	 * @param \Httpful\Response A parsed Httpful Response object.
+	 * @throws \Exception Indicates an error state based on HTTP response codes.
+	 */
+	private static function checkResponseCode($response) {
+		switch($response->code) {
+			case 200:
+				// Ok — everything worked and there's a string array of pwned sites for the account
+				return;
+			case 400:
+				// Bad request — the account does not comply with an acceptable format (i.e. it's an empty string)
+				throw new \Exception("Account must be an email address or user name.");
+				return;
+			case 403:
+				// Forbidden — no user agent has been specified in the request
+				throw new \Exception("No user agent supplied in settings.php");
+				return;
+			case 404:
+				// Not found — the account could not be found and has therefore not been pwned
+				throw new \Exception("Good news! This account is not associated with any breaches.");
+				return;
+		}
+	}
+
+}
+
+// END
