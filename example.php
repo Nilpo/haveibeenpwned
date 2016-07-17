16310e6bb24d279ca13f25d0ff06dd7438d03fb8
Nilpo <devnilpo@gmail.com>
2016-07-17 03:44:51 -0400

    adding example.php

---
+++ example.php
@@ -0,0 +1,35 @@
+<?php
+
+require(__DIR__ . '/vendor/autoload.php');
+
+$result[] = "No results";
+
+if (isset($_POST['account']) && !empty($_POST['account'])) {
+	$account = trim($_POST['account']);
+
+	try {
+		$result = \HIBP\HIBP::getAllBreaches($account);
+	}
+	catch (\Exception $e) {
+		$result[0] = $e->getMessage();
+	}
+}
+
+?>
+
+<form method="POST" action="">
+	<input id="account" name="account" type="email" placeholder="Email or user name">
+	<button id="submit" name="submit" type="submit" value="Submit">Have I Been Pwned?</button>
+</form>
+
+<pre>
+<?php
+
+if (count($result) == 1) :
+	print_r($result[0]);
+else :
+	var_dump($result);
+endif;
+
+?>
+</pre>
